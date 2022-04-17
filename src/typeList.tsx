41934b5bd2c7671a8f5ea040774f47a7b46d82f3
y-masaya <49806793+y-masaya@users.noreply.github.com>
2022-04-18 00:53:34 +0900

    makeFile

---
+++ src/typeList.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**TypeScriptの基本の方 */
+
+//boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+//string
+let str: string = "A";
+
+//Arry
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+//any
+let any1: any = false;
+
+// void
+const funcA = (): vois => {
+  const test = "test";
+};
+
+//null
+let null1: null = null;
+
+//undifined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "AAA" };
